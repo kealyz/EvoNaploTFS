9a8fd9c91e122dac86381cb08dc3547f1f5c4cc9
Márk Magyari <mark.magyari97@gmail.com>
2021-06-22 12:42:05 +0200

    Dropdown

---
+++ EvoNaploTFS/ClientApp/src/components/DropdownMenu/DropdownMenu.js
@@ -0,0 +1,38 @@
+﻿import React, { useState, useRef } from "react";
+
+import './DropdownMenu.css'
+
+function DropdownMenu(props) {
+    const [setActive, setActiveState] = useState("");
+    const [setHeight, setHeightState] = useState("0px");
+    const [setRotate, setRotateState] = useState("accordion__icon");
+
+    const content = useRef(null);
+
+    function toogleMenu() {
+        setActiveState(setActive === "" ? "active" : "");
+        setHeightState(
+            setActive === "active" ? "0px" : `${content.current.scrollHeight}px`
+        );
+        //setRotateState(
+        //    setActive === "active" ? "accordion__icon" : "accordion__icon rotate"
+        //);
+    }
+
+    return (
+        <div className="Dropdownmenu">
+            <button className={`Dropdownmenu ${setActive}`} onClick={toogleMenu}>
+                <p className="DropdownmenuTitle">{props.title}</p>
+            </button>
+            <div
+                ref={content}
+                style={{ maxHeight: `${setHeight}` }}
+                className="DropdownmenuContent"
+            >
+                {props.content}
+            </div>
+        </div>
+    );
+}
+
+export default DropdownMenu;
--- EvoNaploTFS/ClientApp/src/components/NavMenu.js
+++ EvoNaploTFS/ClientApp/src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import DropdownMenu from './DropdownMenu/DropdownMenu';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
@@ -19,6 +20,16 @@ export class NavMenu extends Component {
         this.setState({
             collapsed: !this.state.collapsed
         });
+    }
+
+    GetLists() {
+        return (
+            <div>
+                <a href="/Students">Students</a>
+                <a href="/Mentors">Mentors</a>
+                <a href="/Projects">Projects</a>
+            </div>
+        );
     }
 
     render() {
@@ -56,6 +67,9 @@ export class NavMenu extends Component {
                                 </NavItem>
                                 <NavItem>
                                     <NavLink tag={Link} className="text-light" to="/StartSemester">Start Semester</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <DropdownMenu title="Dropdown" content={this.GetLists} />
                                 </NavItem>
                             </ul>
                         </Collapse>
